65710102b843638165870ebdda7a36807cf4224f
NalbertC <nalberthcastro1510@gmail.com>
2022-05-26 10:32:04 -0300

    Estudos de eventos em JavaScript

---
+++ javascript/15-evento-em-Javascript/personalizado.js
@@ -0,0 +1,11 @@
+function carConteudo() {
+    document.getElementById("conteudo").innerHTML = "Lorem ipsum, dolorsit amet consectetur adipisicing elit. Accusantium modi pariatur, autem fugiat labore est? Repudiandae aspernatur numquam consequatur eligendi aut optio ex commodi,aliquam eos recusandae nisi modi dolorem?";
+}
+
+function mouseOver(){
+    document.getElementById("mouseAlt").innerHTML = "Retire o mouse";
+}
+
+function mouseOut(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse";
+}
